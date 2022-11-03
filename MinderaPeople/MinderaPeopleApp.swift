de1c21ca7ec9e04868d0a7ab5ee38a7d3afa40c1
Pedro Seruca <pedroseruca@gmail.com>
2022-11-03 16:02:56 +0000

    MPA-8: Added AppDelegate

--- MinderaPeople/MinderaPeopleApp.swift
+++ MinderaPeople/MinderaPeopleApp.swift
@@ -5,6 +5,8 @@ import SwiftUI
 
 @main
 struct MinderaPeopleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     private let feature = RootFeature()
 
     var body: some Scene {
