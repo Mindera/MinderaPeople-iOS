b3d5837a7582ac8810b881ba68d2abd830f4132a
Pedro Seruca <pedroseruca@gmail.com>
2022-11-19 18:05:24 +0000

    MPA-8: Code cleanup

--- MinderaPeople/AppDelegate.swift
+++ MinderaPeople/AppDelegate.swift
@@ -13,6 +13,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any])
     -> Bool {
-        return GIDSignIn.sharedInstance.handle(url)
+        GIDSignIn.sharedInstance.handle(url)
     }
 }
--- MinderaPeople/Screens/RootView.swift
+++ MinderaPeople/Screens/RootView.swift
@@ -9,8 +9,6 @@ struct RootFeature: ReducerProtocol {
     struct State: Equatable {
         var signInState: SignInState = .unauthorized
         var alert: AlertState<Action>?
-//
-//        var homeState: HomeFeature.State?
     }
 
     enum SignInState: Equatable {
@@ -21,12 +19,8 @@ struct RootFeature: ReducerProtocol {
     enum Action: Equatable {
         case logInButtonTapped
         case signInResponse(TaskResult<User>)
-        case logOutButtonTapped
-        case signOutResponse
         case homePageDismiss
         case alertDismissTapped
-        
-//        case home(HomeFeature.Action)
     }
 
     @Dependency(\.authenticationService) var authenticationService
@@ -43,27 +37,14 @@ struct RootFeature: ReducerProtocol {
                     )
                 }
 
-            case .signInResponse(let .failure(error)):
-//                state.homeState = nil
-                print(error)
-                guard let authError = error as? AuthenticationServiceError else { return .none }
-                var errorText = ""
-                switch authError {
-                case let .googleSignInFailure(error):
-                    errorText = error
-                case .noAuthenticationToken:
-                    errorText = "noAuthenticationToken"
-                case .noUserFound:
-                    errorText = "noUserFound"
-                case .missingFirebaseClientId:
-                    errorText = "missingFirebaseClientId"
-                }
+            case let .signInResponse(.failure(error)):
+                guard let errorText = authError(from: error) else { return .none }
                 state.alert = AlertState(
-                    title: TextState(errorText),
+                    title: TextState("Something went wrong"),
+                    message: TextState(errorText),
                     primaryButton: .default(TextState("Retry"), action: .send(.logInButtonTapped)),
                     secondaryButton: .cancel(TextState("Ok"), action: .send(.alertDismissTapped))
                 )
-                // TODO: Login failure. Add some feedback to user
                 return .none
 
             case let .signInResponse(.success(user)):
@@ -71,112 +52,84 @@ struct RootFeature: ReducerProtocol {
                 state.signInState = .authorized(user)
                 return .none
 
-            case .logOutButtonTapped:
-                return .task {
-                    try? await authenticationService.signOut()
-                    return .signOutResponse
-                }
-
-            case .signOutResponse:
+            case .homePageDismiss:
                 state.signInState = .unauthorized
                 return .none
 
-            case .homePageDismiss:
-                return .none
-                
             case .alertDismissTapped:
                 state.alert = nil
                 return .none
-                
-//            case .home(.logOutButtonTapped):
-//                return .task {
-//                    try? await authenticationService.signOut()
-//                    return .signOutResponse
-//                }
             }
+        }
+    }
+
+    private func authError(from error: Error) -> String? {
+        guard let authError = error as? AuthenticationServiceError else { return nil }
+        switch authError {
+        case let .googleSignInFailure(error):
+            return error
+        case .noAuthenticationToken:
+            return "noAuthenticationToken"
+        case .noUserFound:
+            return "noUserFound"
+        case .missingFirebaseClientId:
+            return "missingFirebaseClientId"
         }
     }
 }
 
 struct RootView: View {
     let store: StoreOf<RootFeature>
-    var body: some View {
-        NavigationStack {
-            WithViewStore(store) { viewStore in
-                VStack {
-                    Spacer()
-                    VStack {
-                        Image("minderaLogo")
-                            .imageScale(.large)
-                            .foregroundColor(.accentColor)
-                    }
-                    Spacer()
-                    Button {
-                        viewStore.send(.logInButtonTapped)
-                    } label: {
-                        Text("Login with Google Account")
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(Color(hex: "#1040E8"))
-                    }
-                    .frame(height: 40)
-                    .cornerRadius(4)
-                    .alert(
-                        self.store.scope(state: \.alert),
-                        dismiss: .alertDismissTapped
-                    )
-                }
-                .padding()
-                .navigationDestination(
-                    isPresented: viewStore
-                        .binding(
-                            get: { state in
-                                switch state.signInState {
-                                case .unauthorized:
-                                    return false
-                                case .authorized:
-                                    return true
-                                }
-                            },
-                            send: .homePageDismiss
-                        )
-                ) {
-                    let feature = HomeFeature()
-                    HomeView(store: .init(initialState: .init(), reducer: feature.body))
-//                    homePage
-                }
-            }
-        }
+    let viewStore: ViewStoreOf<RootFeature>
+
+    init(store: StoreOf<RootFeature>) {
+        self.store = store
+        viewStore = .init(store)
     }
 
-    // TODO: this should be removed to another view / file
-    @ViewBuilder
-    var homePage: some View {
-        ZStack {
-            WithViewStore(store) { viewStore in
-                Rectangle()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .ignoresSafeArea()
-                    .foregroundColor(.yellow)
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
                 VStack {
-                    Text("Welcome 🚀")
-                        .font(.title)
-                    Spacer()
-                    Button {
-                        viewStore.send(.logOutButtonTapped)
-                    } label: {
-                        Text("Log Out")
-                            .foregroundColor(.yellow)
-                            .padding()
-                            .background {
-                                Capsule()
-                                    .foregroundColor(.black)
+                    Image("minderaLogo")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+                Spacer()
+                Button {
+                    viewStore.send(.logInButtonTapped)
+                } label: {
+                    Text("Login with Google Account")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(hex: "#1040E8"))
+                }
+                .frame(height: 40)
+                .cornerRadius(4)
+                .alert(
+                    self.store.scope(state: \.alert),
+                    dismiss: .alertDismissTapped
+                )
+            }
+            .padding()
+            .navigationDestination(
+                isPresented: viewStore
+                    .binding(
+                        get: { state in
+                            switch state.signInState {
+                            case .unauthorized:
+                                return false
+                            case .authorized:
+                                return true
                             }
-                    }
-                }.padding()
+                        },
+                        send: .homePageDismiss
+                    )
+            ) {
+                HomeView(store: .init(initialState: .init(), reducer: HomeFeature()))
             }
         }
-        .navigationBarBackButtonHidden()
     }
 }
 
@@ -188,7 +141,6 @@ extension RootFeature.State {
 
 struct RootView_Previews: PreviewProvider {
     static var previews: some View {
-        let feature = RootFeature()
-        RootView(store: .init(initialState: .mock(), reducer: feature.body))
+        RootView(store: .init(initialState: .mock(), reducer: RootFeature()))
     }
 }
