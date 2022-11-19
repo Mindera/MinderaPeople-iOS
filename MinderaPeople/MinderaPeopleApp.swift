2aea6af8774913aeb5195dedeb06da387fb6deab
Pedro Seruca <pedroseruca@gmail.com>
2022-11-19 16:36:51 +0000

    MPA-15: Replaced main view back to RootView

--- MinderaPeople/MinderaPeopleApp.swift
+++ MinderaPeople/MinderaPeopleApp.swift
@@ -3,11 +3,11 @@ import SwiftUI
 
 @main
 struct MinderaPeopleApp: App {
-    private let store = StoreOf<BiometricAuthenticationFeature>(initialState: .init(), reducer: BiometricAuthenticationFeature().body)
+    private let store = StoreOf<RootReducer>(initialState: .init(), reducer: RootReducer())
 
     var body: some Scene {
         WindowGroup {
-            LocalAuthenticatorView(store: store)
+            RootView(store: store)
         }
     }
 }
