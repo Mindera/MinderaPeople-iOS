100277bb3bad3755c368fea8bc19469dfb4ed1c9
Daniil Girskiy <daniil.girskiy@mindera.com>
2022-11-30 13:55:08 +0000

    remove comments, alpha/beta environments, follow Mindera Swift Style Guide

--- MinderaPeople/AppDelegate.swift
+++ MinderaPeople/AppDelegate.swift
@@ -1,18 +1,17 @@
-//
-
 import UIKit
 import Firebase
 import GoogleSignIn
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
     }
     
-    func application(_ application: UIApplication, open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any])
-    -> Bool {
+    func application(_ application: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
         GIDSignIn.sharedInstance.handle(url)
     }
 }
