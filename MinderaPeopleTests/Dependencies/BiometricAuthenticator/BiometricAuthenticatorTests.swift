11dec641cd90188ad38b4a415bee22b842e90aaf
Madalina Ardelean <madalina.ardelean@kingfisher.com>
2022-12-21 10:36:36 +0200

    MPA-9: Enhance unit test

--- MinderaPeopleTests/Dependencies/BiometricAuthenticator/BiometricAuthenticatorTests.swift
+++ MinderaPeopleTests/Dependencies/BiometricAuthenticator/BiometricAuthenticatorTests.swift
@@ -4,19 +4,43 @@ import XCTest
 @testable import MinderaPeople
 
 final class BiometricAuthenticatorTests: XCTestCase {
-    private let mockedContext = LAContextMock()
-    private var userDefaultsClient = UserDefaultsClient.testValue
-    private let currentDate = Date()
+    private var mockedContext: LAContextMock!
+    private var userDefaultsClient: UserDefaultsClient!
+    private var currentDate: Date!
+    
+    private var sut: BiometricAuthenticator!
+    
+    override func setUp() {
+        super.setUp()
+        mockedContext = .init()
+        userDefaultsClient = .testValue
+        currentDate = Date()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        currentDate = nil
+        userDefaultsClient = nil
+        mockedContext = nil
+        super.tearDown()
+    }
     
     func testAuthenticationSuccessful() async {
         userDefaultsClient.override(bool: true, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
         userDefaultsClient.override(date: currentDate, forKey: UserDefaultsKeys.lastBiometricAuthenticationDateKey)
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient,
-                                                            referenceDate: currentDate.addingTimeInterval(205))
-        await biometricAuthenticator.setAuthenticationTimeLimit(200)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient,
+            referenceDate: currentDate.addingTimeInterval(205)
+        )
+        
+        await sut.setAuthenticationTimeLimit(200)
         do {
-            let successful = try await biometricAuthenticator.authenticate(force: false)
+            let successful = try await sut.authenticate(force: false)
+            XCTAssertTrue(mockedContext.canEvaluatePolicyCalled)
+            XCTAssertTrue(mockedContext.evaluatePolicyCalled)
+            XCTAssertEqual(mockedContext.lastCanEvaluatePolicyLAPolicy, .deviceOwnerAuthenticationWithBiometrics)
+            XCTAssertEqual(mockedContext.lastEvaluatePolicyLAPolicy, .deviceOwnerAuthenticationWithBiometrics)
             XCTAssertTrue(successful)
         } catch {
             XCTFail("No errors should be received")
@@ -26,12 +50,14 @@ final class BiometricAuthenticatorTests: XCTestCase {
     func testSuccessfulAuthenticationWithinTimeLimit() async {
         userDefaultsClient.override(bool: true, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
         userDefaultsClient.override(date: currentDate, forKey: UserDefaultsKeys.lastBiometricAuthenticationDateKey)
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient,
-                                                            referenceDate: currentDate.addingTimeInterval(150))
-        await biometricAuthenticator.setAuthenticationTimeLimit(200)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient,
+            referenceDate: currentDate.addingTimeInterval(150)
+        )
+        await sut.setAuthenticationTimeLimit(200)
         do {
-            let successful = try await biometricAuthenticator.authenticate(force: false)
+            let successful = try await sut.authenticate(force: false)
             XCTAssertTrue(successful)
         } catch {
             XCTFail("No errors should be received")
@@ -41,10 +67,14 @@ final class BiometricAuthenticatorTests: XCTestCase {
     
     func testBiometricAuthenticationNotEnabled() async {
         userDefaultsClient.override(bool: false, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient
+        )
         do {
-            let successful = try await biometricAuthenticator.authenticate(force: false)
+            let successful = try await sut.authenticate(force: false)
+            XCTAssertFalse(mockedContext.canEvaluatePolicyCalled)
+            XCTAssertFalse(mockedContext.evaluatePolicyCalled)
             XCTAssertFalse(successful)
         } catch {
             XCTFail("No errors should be received")
@@ -53,10 +83,12 @@ final class BiometricAuthenticatorTests: XCTestCase {
     
     func testForcedAuthentication() async {
         userDefaultsClient.override(bool: true, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient
+        )
         do {
-            let successful = try await biometricAuthenticator.authenticate(force: true)
+            let successful = try await sut.authenticate(force: true)
             XCTAssertTrue(successful)
         } catch {
             XCTFail("No errors should be received")
@@ -66,11 +98,15 @@ final class BiometricAuthenticatorTests: XCTestCase {
     func testBiometricNotEnrolled() async {
         userDefaultsClient.override(bool: true, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
         mockedContext.canEvaluatePolicyResponse = false
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient
+        )
         do {
-            let _ = try await biometricAuthenticator.authenticate(force: true)
+            let _ = try await sut.authenticate(force: true)
         } catch {
+            XCTAssertTrue(mockedContext.canEvaluatePolicyCalled)
+            XCTAssertFalse(mockedContext.evaluatePolicyCalled)
             XCTAssertNotNil(error)
             XCTAssert(error is BiometricAuthenticatorError)
         }
@@ -79,10 +115,12 @@ final class BiometricAuthenticatorTests: XCTestCase {
     func testBiometricAuthenticationFailedFaceRecognition() async {
         userDefaultsClient.override(bool: true, forKey: UserDefaultsKeys.biometricAuthenticationEnabled)
         mockedContext.evaluatePolicyResponse = false
-        let biometricAuthenticator = BiometricAuthenticator(authenticationContext: mockedContext,
-                                                            userDefaults: userDefaultsClient)
+        sut = BiometricAuthenticator(
+            authenticationContext: mockedContext,
+            userDefaults: userDefaultsClient
+        )
         do {
-            let successful = try await biometricAuthenticator.authenticate(force: true)
+            let successful = try await sut.authenticate(force: true)
             XCTAssertFalse(successful)
         } catch {
             XCTFail("No errors should be received")
--- MinderaPeopleTests/Dependencies/BiometricAuthenticator/LAContextMock.swift
+++ MinderaPeopleTests/Dependencies/BiometricAuthenticator/LAContextMock.swift
@@ -5,11 +5,25 @@ class LAContextMock: LAContext {
     var canEvaluatePolicyResponse = true
     var evaluatePolicyResponse = true
     
+    // MARK: - canEvaluatePolicy vars
+    private(set) var canEvaluatePolicyCalled = false
+    private(set) var lastCanEvaluatePolicyLAPolicy: LAPolicy!
+    
+    // MARK: - evaluatePolicy vars
+    private(set) var evaluatePolicyCalled = false
+    private(set) var lastEvaluatePolicyLAPolicy: LAPolicy!
+    
     override func canEvaluatePolicy(_ policy: LAPolicy, error: NSErrorPointer) -> Bool {
+        canEvaluatePolicyCalled = true
+        lastCanEvaluatePolicyLAPolicy = policy
+
         return canEvaluatePolicyResponse
     }
     
     override func evaluatePolicy(_ policy: LAPolicy, localizedReason: String) async throws -> Bool {
+        evaluatePolicyCalled = true
+        lastEvaluatePolicyLAPolicy = policy
+
         return evaluatePolicyResponse
     }
 }
