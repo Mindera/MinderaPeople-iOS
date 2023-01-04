770c3f4484d1f9135b842c194b1cb23231815d5e
Daniil Girskiy <daniil.girskiy@mindera.com>
2023-01-04 13:06:54 +0000

    MPA-28: add toolbar functionality

--- MinderaPeople/Screens/Home/HomeView.swift
+++ MinderaPeople/Screens/Home/HomeView.swift
@@ -1,24 +1,45 @@
 import ComposableArchitecture
+import MinderaDesignSystem
 import SwiftUI
 
 struct HomeView: View {
     let store: StoreOf<Home>
-
+    
     init(store: StoreOf<Home>) {
         self.store = store
     }
-
+    
     var body: some View {
         WithViewStore(self.store) { viewStore in
-            ZStack {
-                Rectangle()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .ignoresSafeArea()
-                    .foregroundColor(.yellow)
-                VStack {
-                    Text("Welcome 🚀")
-                        .font(.title)
-                    Spacer()
+            TabBarView(
+                selection: viewStore.binding(
+                    get: { $0.selectedTab },
+                    send: { .tabButtonPressed($0) }
+                )
+            )
+            .tab(
+                .dashboard,
+                content: {
+                    Text("Dashboard")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.red(._100))
+                },
+                icon: icon(.dashboard, isHighlighted: viewStore.selectedTab == .dashboard),
+                text: text(.dashboard, isHighlighted: viewStore.selectedTab == .dashboard)
+            )
+            .tab(
+                .calendar,
+                content: {
+                    Text("Calendar")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.yellow(._100))
+                },
+                icon: icon(.calendar, isHighlighted: viewStore.selectedTab == .calendar),
+                text: text(.calendar, isHighlighted: viewStore.selectedTab == .calendar)
+            )
+            .tab(
+                .myProfile,
+                content: {
                     Button {
                         viewStore.send(.logOutButtonTapped)
                     } label: {
@@ -30,11 +51,45 @@ struct HomeView: View {
                                     .foregroundColor(.black)
                             }
                     }
-                }
-                .padding()
-            }
+                },
+                icon: icon(.myProfile, isHighlighted: viewStore.selectedTab == .myProfile),
+                text: text(.myProfile, isHighlighted: viewStore.selectedTab == .myProfile)
+            )
+            .shadow(radius: 1)
             .navigationBarBackButtonHidden()
         }
+    }
+    
+    private func icon(_ selectionValue: Home.Tab, isHighlighted: Bool) -> some View {
+        var name = ""
+        switch selectionValue {
+        case .dashboard:
+            name = "dashboard"
+        case .calendar:
+            name = "calendar"
+        case .myProfile:
+            name = "myProfile"
+        }
+        return Image(name)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+            .foregroundColor(isHighlighted ? Color.indigo(._600) : Color.greyBlue(._500))
+    }
+    
+    private func text(_ selectionValue: Home.Tab, isHighlighted: Bool) -> some View {
+        var text = ""
+        switch selectionValue {
+        case .dashboard:
+            text = "Dashboard"
+        case .calendar:
+            text = "Calendar"
+        case .myProfile:
+            text = "My Profile"
+        }
+        return Text(text)
+            .customFont(size: .XS)
+            .foregroundColor(isHighlighted ? Color.indigo(._600) : Color.greyBlue(._500))
     }
 }
 
