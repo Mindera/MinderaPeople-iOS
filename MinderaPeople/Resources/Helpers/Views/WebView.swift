d189c3e3879f9fb8c5fea518950ce32d42faff40
Daniil Girskiy <daniil.girskiy@mindera.com>
2023-01-05 19:03:14 +0000

    MPA-8: add custom agent to web view

--- MinderaPeople/Resources/Helpers/Views/WebView.swift
+++ MinderaPeople/Resources/Helpers/Views/WebView.swift
@@ -8,6 +8,7 @@ struct WebView: UIViewRepresentable {
 
     init(request: URLRequest, completion: @escaping (String) -> Void) {
         webView = WKWebView()
+        webView.customUserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
         self.request = request
         self.completion = completion
     }
